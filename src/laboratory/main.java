837647de7496e167e7cf6c4988470e4f852d88f8
Ilya <86732040+GitIcon65@users.noreply.github.com>
2022-07-24 22:24:36 +0300

    Update main.java

--- src/laboratory/main.java
+++ src/laboratory/main.java
@@ -78,7 +78,7 @@ public class main extends Plugin {
             String monster = member.get(randomIndex);
 
             Player player = Groups.player.find(p -> p.uuid().equals(monster));
-            player.team(crux.team());
+            player.team(team.crux());
 
             Call.infoToast("Game started", 10f);
         });
